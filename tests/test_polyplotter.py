6e6ca1ce48fbd132d2e539c4722daee3f16d3fec
Julius <julius.simonelli@gmail.com>
2023-05-31 17:30:54 -0700

    Update test_polyplotter.py

--- tests/test_polyplotter.py
+++ tests/test_polyplotter.py
@@ -1,28 +1,29 @@
 """
-TODO: Write these tests
+TODO: Add more tests
 """
-import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import pytest
-import shapely
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Polygon
 from shapely import wkt  # required to access shapely.wkt
-from shapely.geometry import LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon, box
 
 from polyplotter import plotpoly as p
+
+matplotlib.use("Agg")  # non-iteractive usage so the graphs don't pop up
 
 
 def test_empty_geom_collection():
     empty_shape = wkt.loads("GEOMETRYCOLLECTION EMPTY")
+    p(empty_shape)
 
 
 def test_simple_wkt():
     simple_poly_wkt = "POLYGON ((45 58, 46 59, 57 55, 54 46, 44 49, 46 53, 44 54, 45 58))"
+    p(simple_poly_wkt)
 
 
 def test_square_wkt():
     square_wkt = "POLYGON ((4 5, 5 5, 5 4, 4 4, 4 5))"
+    p(square_wkt)
 
 
 def test_simple_poly():
@@ -30,6 +31,7 @@ def test_simple_poly():
     simple_poly = wkt.loads(simple_poly_wkt)
     empty_shape = wkt.loads("GEOMETRYCOLLECTION EMPTY")
     empty_geometry_collection = empty_shape.intersection(simple_poly)
+    p(empty_geometry_collection)
 
 
 def test_ndarray_coords():
@@ -46,26 +48,7 @@ def test_ndarray_coords():
         ],
         dtype=np.int32,
     )
-
-    soln = p(coords)
-
-    # Create a polygon and add it to a patch collection
-    polygon = Polygon(coords)
-    patches = [polygon]
-
-    # Create figure and axes
-    fig, ax = plt.subplots()
-
-    # Create a patch collection. With facecolor set to none, this will be a line plot.
-    p = PatchCollection(patches, facecolor="none", edgecolor="black")
-
-    # Add the collection to the plot
-    ax.add_collection(p)
-
-    # Automatically adjust view to the data
-    ax.autoscale_view()
-
-    plt.show()
+    p(coords)
 
 
 if __name__ == "__main__":
